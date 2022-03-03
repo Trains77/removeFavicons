e247c2c78652c5a265db18756512d3c64752300f
Trains77 <trains77videos123@gmail.com>
2022-03-03 12:41:51 -0500

    Minor changes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pathlib, os
 from pathlib import Path
 
-librewolf1 = os.getenv("HOME") + "/.librewolf/7nojjl8s.default-release/favicons.sqlite"
+librewolf_instance = "7nojjl8s.default-release"
+
+librewolf1 = os.getenv("HOME") + "/.librewolf/" + librewolf_instance +  "/favicons.sqlite"
 isFile = os.path.isfile(librewolf1)
 if isFile == True:
     os.remove(librewolf1)
-librewolf2 = os.getenv("HOME") + "/.librewolf/7nojjl8s.default-release/favicons.sqlite-wal"
+librewolf2 = os.getenv("HOME") + "/.librewolf/" + librewolf_instance + "/favicons.sqlite-wal"
 isFile = os.path.isfile(librewolf2)
 if isFile == True:
     os.remove(librewolf2)
