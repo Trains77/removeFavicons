819d8a43837c21e7e5741f7b1146cc02ee19e494
Trains77 <trains77videos123@gmail.com>
2022-03-04 10:53:15 -0500

    Support for Firefox and Brave

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This is a simple script I made that removes Favicons from my Librewolf browser instance
 
 This script only works on Linux systems
+
+Works with Firefox, Librewolf, and Brave (Non-Flatpak versions)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pathlib, os
 from pathlib import Path
 
 librewolf_instance = "7nojjl8s.default-release"
+firefox_instance = "7ktl4nut.default-release"
 
 librewolf1 = os.getenv("HOME") + "/.librewolf/" + librewolf_instance +  "/favicons.sqlite"
 isFile = os.path.isfile(librewolf1)
@@ -11,3 +12,21 @@ librewolf2 = os.getenv("HOME") + "/.librewolf/" + librewolf_instance + "/favicon
 isFile = os.path.isfile(librewolf2)
 if isFile == True:
     os.remove(librewolf2)
+
+firefox1 = os.getenv("HOME") + "/.mozilla/firefox/" + firefox_instance +  "/favicons.sqlite"
+isFile = os.path.isfile(firefox1)
+if isFile == True:
+    os.remove(firefox1)
+firefox2 = os.getenv("HOME") + "/.mozilla/firefox/" + firefox_instance + "/favicons.sqlite-wal"
+isFile = os.path.isfile(firefox2)
+if isFile == True:
+    os.remove(firefox2)
+
+brave1 = os.getenv("HOME") + ".config/BraveSoftware/Default/Favicons"
+isFile = os.path.isfile(brave1)
+if isFile == True:
+    os.remove(brave1)
+brave2 = os.getenv("HOME") + ".config/BraveSoftware/Default/Favicons-journal"
+isFile = os.path.isfile(brave2)
+if isFile == True:
+    os.remove(brave2)
