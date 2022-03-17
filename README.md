5d396f85a3e9f07993b9be3afad9299de25a7dda
Trains77 <trains77videos123@gmail.com>
2022-03-17 08:57:23 -0400

    Added chrome support and fixed a bug with Brave favicons not being removed

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ This is a simple script I made that removes Favicons from my Librewolf browser i
 
 This script only works on Linux systems
 
-Works with Firefox, Librewolf, and Brave (Non-Flatpak versions)
+Works with Firefox, Librewolf, Brave, and Chrome (Non-Flatpak versions)
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pathlib, os
 from pathlib import Path
 
+# Set these to the actual name of your Firefox or Librewolf profiles
 librewolf_instance = "7nojjl8s.default-release"
 firefox_instance = "7ktl4nut.default-release"
+
+# Librewolf
 
 librewolf1 = os.getenv("HOME") + "/.librewolf/" + librewolf_instance +  "/favicons.sqlite"
 isFile = os.path.isfile(librewolf1)
@@ -13,6 +16,8 @@ isFile = os.path.isfile(librewolf2)
 if isFile == True:
     os.remove(librewolf2)
 
+# Firefox
+
 firefox1 = os.getenv("HOME") + "/.mozilla/firefox/" + firefox_instance +  "/favicons.sqlite"
 isFile = os.path.isfile(firefox1)
 if isFile == True:
@@ -22,11 +27,24 @@ isFile = os.path.isfile(firefox2)
 if isFile == True:
     os.remove(firefox2)
 
-brave1 = os.getenv("HOME") + ".config/BraveSoftware/Default/Favicons"
+# Brave
+
+brave1 = os.getenv("HOME") + "/.config/BraveSoftware/Default/Favicons"
 isFile = os.path.isfile(brave1)
 if isFile == True:
     os.remove(brave1)
-brave2 = os.getenv("HOME") + ".config/BraveSoftware/Default/Favicons-journal"
+brave2 = os.getenv("HOME") + "/.config/BraveSoftware/Default/Favicons-journal"
 isFile = os.path.isfile(brave2)
 if isFile == True:
     os.remove(brave2)
+
+# Chrome
+chrome1 = os.getenv("HOME") + "/.config/google-chrome/Default/Favicons"
+isFile = os.path.isfile(chrome1)
+if isFile == True:
+    os.remove(chrome1)
+chrome2 = os.getenv("HOME") + "/.config/google-chrome/Default/Favicons-journal"
+isFile = os.path.isfile(chrome2)
+if isFile == True:
+    os.remove(chrome2)
+os.system("clear")
